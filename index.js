b5f80330218d458ea70df84d169e3b4c699c14b7
simpart <simpart>
2018-08-01 08:46:28 +0000

    supported prm map

--- index.js
+++ index.js
@@ -13,25 +13,8 @@ mofron.effect.HrzPos = class extends mofron.Effect {
         try {
             super();
             this.name('HrzPos');
+            this.prmMap('type');
             this.prmOpt(po);
-            
-            this.getParam().check(
-                (pos) => {
-                    try {
-                        if ('string' !== typeof pos) {
-                            throw new Error('invalid parameter');
-                        }
-                        if ( ('left'   !== pos) &&
-                             ('center' !== pos) &&
-                             ('right'  !== pos) ) {
-                            throw new Error('invalid parameter');
-                        }
-                    } catch (e) {
-                        console.error(e.stack);
-                        throw e;
-                    }
-                }
-            );
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -40,24 +23,29 @@ mofron.effect.HrzPos = class extends mofron.Effect {
     
     enable (cmp) {
         try {
-            let type = this.value()[0];
-            if ('left' === type) {
+            if (true === mofron.func.isInclude(cmp, 'Text')) {
+                cmp.style({
+                    'text-align' : this.type()
+                });
+                return;
+            }
+            
+            if ('left' === this.type()) {
                 cmp.style({
                     'margin-right' : 'auto',
                     'margin-left'  : '0px'
                 });
-            } else if ('center' === type) {
+            } else if ('center' === this.type()) {
                 cmp.style({
                     'margin-right' : 'auto',
                     'margin-left'  : 'auto'
                 });
-            } else if ('right' === type) {
+            } else if ('right' === this.type()) {
                 cmp.style({
                     'margin-right' : '0px',
                     'margin-left'  : 'auto'
                 });
             }
-            
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -66,10 +54,38 @@ mofron.effect.HrzPos = class extends mofron.Effect {
     
     disable (cmp) {
         try {
-            cmp.style({
-                'margin-right' : null,
-                'margin-left'  : null
-            });
+            if (true === mofron.func.isInclude(cmp, 'Text')) {
+                cmp.style({
+                    'text-align' : null
+                });
+            } else {
+                cmp.style({
+                    'margin-right' : null,
+                    'margin-left'  : null
+                });
+            }
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    type (prm) {
+        try {
+            if (undefined === prm) {
+                /* getter */
+                return (undefined === this.m_type) ? 'center' : this.m_type;
+            }
+            /* setter */
+            if ('string' !== typeof prm) {
+                throw new Error('invalid parameter');
+            }
+            if ( ('left'   !== prm) &&
+                 ('center' !== prm) &&
+                 ('right'  !== prm) ) {
+                throw new Error('invalid parameter');
+            }
+            this.m_type = prm;
         } catch (e) {
             console.error(e.stack);
             throw e;
