363ca7c271a997e065a2a29c150cd61f91e0e0da
simpart <simpart>
2018-10-18 13:31:47 +0000

    add comment

--- index.js
+++ index.js
@@ -9,11 +9,18 @@ const mf = require('mofron');
  */
 mf.effect.HrzPos = class extends mf.Effect {
     
+    /**
+     * initialize effect
+     *
+     * @param p1 (object) effect option
+     * @param p1 (string) type function parameter
+     * @param p2 (string) offset function parameter
+     */
     constructor (po, p2) {
         try {
             super();
             this.name('HrzPos');
-            this.prmMap('type', 'offset');
+            this.prmMap(['type', 'offset']);
             this.prmOpt(po, p2);
         } catch (e) {
             console.error(e.stack);
@@ -21,22 +28,38 @@ mf.effect.HrzPos = class extends mf.Effect {
         }
     }
     
-    enable (cmp) {
+    /**
+     * effect contents
+     *
+     * @note private method
+     */
+    contents (flg, cmp) {
         try {
             if (true === mf.func.isInclude(cmp, 'Text')) {
-                this.text(cmp,true);
+                this.textPos(cmp, flg);
             } else {
-                this.other(cmp, true);
+                this.otherPos(cmp, flg);
             }
+            super.contents(flg, cmp);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    text (cmp, flg) {
+    enable (cmp) {}
+    disable (cmp) {}
+    
+    /**
+     * execute text component position
+     * 
+     * @note private method
+     */
+    textPos (cmp, flg) {
         try {
-            if ((null !== cmp.target().parent()) && ('flex' === cmp.target().parent().style('display'))) {
+            if ( (null !== cmp.target().parent()) &&
+                 ('flex' === cmp.target().parent().style('display')) ) {
+                
                 if ('center' === this.type()) {
                     cmp.style({
                         'margin-right' : (true === flg) ? 'auto' : null,
@@ -51,25 +74,13 @@ mf.effect.HrzPos = class extends mf.Effect {
                 } else if ('left' === this.type()) {
                     cmp.style({
                         'margin-right' : (true === flg) ? 'auto' : null,
-                        'margin-left'  : (true === flg) ? '0rem' : null
+                        'margin-left'  : (true === flg) ? mf.func.sizeSum('0rem', this.offset()) : null
                     });
-                    if ((true === flg) && (null !== this.offset())) {
-                        cmp.sizeValue(
-                            'margin-left',
-                            mf.func.sizeSum('0rem', this.offset())
-                        );
-                    }
                 } else if ('right' === this.type()) {
                     cmp.style({
-                        'margin-right' : (true === flg) ? '0rem' : null,
+                        'margin-right' : (true === flg) ? mf.func.sizeSum('0rem', this.offset()) : null,
                         'margin-left'  : (true === flg) ? 'auto' : null
                     });
-                    if ((true === flg) && (null !== this.offset())) {
-                        cmp.sizeValue(
-                            'margin-right', 
-                            mf.func.sizeSum('0rem', this.offset())
-                        );
-                    }
                 }
             } else {
                 cmp.style({
@@ -82,61 +93,32 @@ mf.effect.HrzPos = class extends mf.Effect {
         }
     }
     
-    other (cmp, flg) {
+    /**
+     * execute other component position
+     *
+     * @note private method
+     */
+    otherPos (cmp, flg) {
         try {
-            if ('left' === this.type()) {
+            if ('center' === this.type()) {
+                this.otherPosCenter(cmp, flg);
+            } else if ('left' === this.type()) {
                 if ('absolute' === cmp.style('position')) {
-                    cmp.style({ 'left' : (true === flg) ? '0rem' : null });
+                    cmp.style({ 'left' : (true === flg) ? mf.func.sizeSum('0rem', this.offset()) : null });
                 } else {
                     cmp.style({
                         'margin-right' : (true === flg) ? 'auto' : null,
-                        'margin-left'  : (true === flg) ? '0rem' : null
-                    });
-                }
-            } else if ('center' === this.type()) {
-                
-                if (null === cmp.parent()) {
-                    cmp.parentLitener(
-                        (p1, p2) => {
-                            try { p2.execute(); } catch (e) {
-                                console.error(e.stack);
-                                throw e;
-                            }
-                        },
-                        this
-                    );
-                } else if ( (0 !== cmp.sizeValue('width').value()) &&
-                            ( ('%' === cmp.parent().sizeValue('width').type()) ||
-                              (0 !== cmp.parent().sizeValue('width').value()) ) ) {
-                    cmp.style({ 'position' : 'relative' }, true);
-                    cmp.style({
-                        'margin-left' : '50%',
-                        'left'        : '-' + cmp.sizeValue('width').value()/2 + cmp.sizeValue('width').type()
-                    });
-                    if ((true === flg) && (null !== this.offset())) {
-                        cmp.sizeValue(
-                            'left',
-                            mf.func.sizeSum(
-                                cmp.sizeValue('left'),
-                                this.offset()
-                            )
-                        );
-                    }
-                    
-                } else {
-                    cmp.style({
-                        'margin-right' : (true === flg) ? 'auto' : null,
-                        'margin-left'  : (true === flg) ? 'auto' : null
+                        'margin-left'  : (true === flg) ? mf.func.sizeSum('0rem', this.offset()) : null
                     });
                 }
             } else if ('right' === this.type()) {
                 if ('absolute' === cmp.style('position')) {
                     cmp.style({
-                        'right' : (true === flg) ? '0rem' : null,
+                        'right' : (true === flg) ? mf.func.sizeSum('0rem', this.offset()) : null
                     });
                 } else {
                     cmp.style({
-                        'margin-right' : (true === flg) ? '0rem' : null,
+                        'margin-right' : (true === flg) ? mf.func.sizeSum('0rem', this.offset()) : null,
                         'margin-left'  : (true === flg) ? 'auto' : null
                     });
                 }
@@ -147,12 +129,51 @@ mf.effect.HrzPos = class extends mf.Effect {
         }
     }
     
-    disable (cmp) {
+    /**
+     * execute other component center position
+     *
+     * @note private method
+     */
+    otherPosCenter (cmp, flg) {
         try {
-            if (true === mf.func.isInclude(cmp, 'Text')) {
-                this.text(cmp, false);
+            if (null === cmp.parent()) {
+                cmp.parentLitener(
+                    (p1, p2) => {
+                        try { p2.execute(); } catch (e) {
+                            console.error(e.stack);
+                            throw e;
+                        }
+                    },
+                    this
+                );
+            } else if ( (0 !== cmp.sizeValue('width').value()) &&
+                        ( ('%' === cmp.parent().sizeValue('width').type()) ||
+                          (0 !== cmp.parent().sizeValue('width').value()) ) ) {
+                cmp.style({ 'position' : 'relative' }, true);
+                cmp.style({
+                    'margin-left' : '50%',
+                    'left'        : null
+                });
+                if (true === flg) {
+                    cmp.sizeValue(
+                        'left',
+                        mf.func.sizeSum(
+                            '-' + cmp.sizeValue('width').value()/2 + cmp.sizeValue('width').type(),
+                            this.offset()
+                        )
+                    );
+                }
             } else {
-                this.other(cmp, false);
+                cmp.style({
+                    'margin-right' : (true === flg) ? 'auto' : null,
+                    'margin-left'  : (true === flg) ? 'auto' : null
+                });
+                if ( (true === flg) && (null !== this.offset()) ) {
+                    cmp.style({
+                        'position' : 'relative',
+                        'left'     : this.offset()
+                    });
+                }
             }
         } catch (e) {
             console.error(e.stack);
@@ -160,40 +181,31 @@ mf.effect.HrzPos = class extends mf.Effect {
         }
     }
     
+    /**
+     * setter/getter position type 
+     * 
+     * @param p1 ('center', 'left', 'right') set position type
+     * @param p1 (undefind) call as getter
+     * @return (string) position type
+     */
     type (prm) {
         try {
-            if (undefined === prm) {
-                /* getter */
-                return (undefined === this.m_type) ? 'center' : this.m_type;
-            }
-            /* setter */
-            if ('string' !== typeof prm) {
-                throw new Error('invalid parameter');
-            }
-            if ( ('left'   !== prm) &&
-                 ('center' !== prm) &&
-                 ('right'  !== prm) ) {
-                throw new Error('invalid parameter');
-            }
-            this.m_type = prm;
+            return this.member('type', ['center', 'left', 'right'], prm, 'center');
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
+    /**
+     * setter/getter offset position
+     * 
+     * @param p1 (string) size value of css
+     * @param p1 (undefined) call as getter
+     * @return (string) size value of css
+     */
     offset (prm) {
-        try {
-            if (undefined === prm) {
-                /* getter */
-                return (undefined === this.m_offset) ? null : this.m_offset;
-            }
-            /* setter */
-            if ('string' !== typeof prm) {
-                throw new Error('invalid parameter');
-            }
-            this.m_offset = prm;
-        } catch (e) {
+        try { return this.member('offset', 'string', prm, '0rem'); } catch (e) {
             console.error(e.stack);
             throw e;
         }
